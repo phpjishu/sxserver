80a8fa56bda347bfa6f9e2ee20540af25a8324a0
phpjishu <phpjishu@qq.com>
2018-09-18 18:28:14 +0800

    删除例子组件

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,12 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'indent': 0,
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'vue/no-parsing-error': [2, {'x-invalid-end-tag': false}],
     'no-undef': 'off'
   }
 }
